c7bf1745603be6d3de87127fc6cf94fc31ca0cab
John Rose <johnr909@yahoo.com>
2022-03-11 23:30:28 -0700

    cleanup pre-deploy commit

--- deploy.php
+++ deploy.php
@@ -4,10 +4,10 @@ namespace Deployer;
 require 'recipe/symfony.php';
 
 // Project name
-set('application', 'my_project');
+set('application', 'symfony_quick_tour_base');
 
 // Project repository
-set('repository', '/home/johnr909/symfony_quick_tour_base/.git');
+set('repository', 'git@github.com:johnr909/symfony_quick_tour_base.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', true); 
@@ -16,19 +16,32 @@ set('git_tty', true);
 add('shared_files', []);
 add('shared_dirs', []);
 
-// Writable dirs by web server 
-add('writable_dirs', []);
+set('clear_paths_use_sudo', true);
+set('clear_paths', [
+    '.git',
+    '.gitignore',
+    '.env.local',
+    'var',
+    'vendor',
+    'symfony_quick_tour_base.sublime-project',
+    'symfony_quick_tour_base.sublime-workspace',
+]);
 
+// Writable dirs by web server 
+add('writable_dirs', ['/var/www/html/symfony-skel']);
 
 // Hosts
 
-host('project.com')
-    ->set('deploy_path', '~/{{application}}');    
+host('symfony.skel')
+    ->set('deploy_path', '/var/www/html/symfony-skel');    
     
 // Tasks
 
 task('build', function () {
     run('cd {{release_path}} && build');
+    run("chown -R www-data:web-dev /var/www/html/symfony-skel");
+    writeln('<info>Deployment is done.</info>');
+
 });
 
 // [Optional] if deploy fails automatically unlock.
